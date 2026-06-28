23773c6f4d276d7e82e04839a701a0f72f01b69a
tngmid <156785319+tngmid@users.noreply.github.com>
2026-06-28 16:11:07 +0100

    Update website.py

--- website.py
+++ website.py
@@ -7,10 +7,4 @@ st.title("NEXT Group Sales Analysis")
 
 df = pd.read_csv("group_sales.csv")
 
-chart = (alt.Chart(df)
-                 .mark_area(opacity=0.6)
-                 .encode(
-                     x="Year"
-                     )
-                 )
-st.altair_chart(chart)
+st.line_chart(df)
