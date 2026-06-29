0dc5d0c1cc64cdec0b36f3098e8770e15342ce70
tngmid <156785319+tngmid@users.noreply.github.com>
2026-06-29 10:45:27 +0100

    Update website.py

--- website.py
+++ website.py
@@ -6,4 +6,13 @@ st.title("NEXT Group Sales Analysis")
 df = pd.read_csv("group_sales.csv")
 df["Year"] = pd.to_datetime(df["Year"], format="%Y")
 
+area = st.selectbox("Select Crop Type:", ["<select>", "Retail Stores", "Online (UK)", "Online (International)", "NEXT Finance", "Other business activities", "NEXT's share of sales from investments", "Total Group sales"])
+
+if area != "<select>":
+    st.sidebar.header(crop)
+    cbAll = st.sidebar.checkbox("Select All")
+else:
+    st.write("Please select an area of interest.")
+
+
 st.line_chart(df, x="Year")
