4b1b7c505a38bb7f2549f1e63a77cdbdc2a2b285
tngmid <156785319+tngmid@users.noreply.github.com>
2026-06-29 10:47:16 +0100

    Update website.py

--- website.py
+++ website.py
@@ -12,7 +12,7 @@ df["Year"] = pd.to_datetime(df["Year"], format="%Y")
 area = st.selectbox("Select Crop Type:", ["<select>", "Retail Stores", "Online (UK)", "Online (International)", "NEXT Finance", "Other business activities", "NEXT's share of sales from investments", "Total Group sales"])
 
 if area != "<select>":
-    st.sidebar.header(crop)
+    st.sidebar.header(area)
     cbAll = st.sidebar.checkbox("Select All")
 else:
     st.write("Please select an area of interest.")
