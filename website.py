14b120bf9e5ed20e31f1b24d184ba3fb4ba4466b
tngmid <156785319+tngmid@users.noreply.github.com>
2026-07-03 11:51:14 +0100

    Update website.py

--- website.py
+++ website.py
@@ -14,8 +14,6 @@ df["Year"] = pd.to_datetime(df["Year"], format="%Y")
 area_list = ["Retail Stores", "Online", "NEXT Finance", "Other business activities", "NEXT's share of sales from investments", "Total Group sales"]
 cbAll = st.sidebar.checkbox("Select All")
 
-st.write(df)
-
 if cbAll:
     selected_layers = [type for type in area_list if st.sidebar.checkbox(type, value=cbAll, disabled=True)]
 else:
