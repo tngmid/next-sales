998bd474989a7aa72ffad5baa370ed7bff28e7ba
tngmid <156785319+tngmid@users.noreply.github.com>
2026-07-03 11:48:16 +0100

    Update website.py

--- website.py
+++ website.py
@@ -8,9 +8,7 @@ with st.sidebar:
 st.title("NEXT Group Sales Analysis")
 
 df = pd.read_csv("group_sales.csv")
-st.write(df)
-df.reset_index(level=0, inplace=True)
-st.write(df)
+df["Year"] = ["2026", "2025", "2025", "2024"]
 df["Year"] = pd.to_datetime(df["Year"], format="%Y")
 
 area_list = ["Retail Stores", "Online (UK)", "Online (International)", "NEXT Finance", "Other business activities", "NEXT's share of sales from investments", "Total Group sales"]
